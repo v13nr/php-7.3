c01c320f5a2c342f39b015c5395455cab89bf843
v13nr2 <nanang.programmer@gmail.com>
2019-05-24 05:17:18 +0800

    db table, users

---
+++ db/users.sql
@@ -0,0 +1,47 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versi server:                 10.1.37-MariaDB - mariadb.org binary distribution
+-- OS Server:                    Win32
+-- HeidiSQL Versi:               10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- membuang struktur untuk table php7.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `oauth_uid` text,
+  `oauth_provider` varchar(100) DEFAULT NULL,
+  `pass` varchar(64) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `full_name` varchar(200) NOT NULL,
+  `avatar` text NOT NULL,
+  `banned` tinyint(1) DEFAULT '0',
+  `last_login` datetime DEFAULT NULL,
+  `last_activity` datetime DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `forgot_exp` text,
+  `remember_time` datetime DEFAULT NULL,
+  `remember_exp` text,
+  `verification_code` text,
+  `top_secret` varchar(16) DEFAULT NULL,
+  `ip_address` text,
+  `seksi_user` varchar(200) NOT NULL DEFAULT 'Pendis',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Membuang data untuk tabel php7.users: 1 rows
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `email`, `oauth_uid`, `oauth_provider`, `pass`, `username`, `full_name`, `avatar`, `banned`, `last_login`, `last_activity`, `date_created`, `forgot_exp`, `remember_time`, `remember_exp`, `verification_code`, `top_secret`, `ip_address`, `seksi_user`) VALUES
+	(1, 'nanang.manajer@gmail.com', NULL, NULL, '21232f297a57a5a743894a0e4a801fc3', 'admin', 'Nanang Rustianto', '', 0, '2019-05-24 05:15:21', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Pendis');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
